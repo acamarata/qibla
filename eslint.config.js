88d7dacaf2efce5f1aa5cfb73086b7fed875118e
Aric Camarata <aric.camarata@gmail.com>
2026-05-31 08:47:15 -0400

    ci: fix eslint parser devDeps and typed-linting config

--- eslint.config.js
+++ eslint.config.js
@@ -2,15 +2,29 @@ import tsParser from '@typescript-eslint/parser';
 import tsPlugin from '@typescript-eslint/eslint-plugin';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import { typescript } from '@acamarata/eslint-config';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default [
   {
+    files: ['src/**/*.ts'],
     plugins: { '@typescript-eslint': tsPlugin },
-    languageOptions: { parser: tsParser },
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+    },
   },
-  ...typescript,
+  {
+    files: ['src/**/*.ts'],
+    ...typescript.reduce((acc, cfg) => ({ ...acc, ...cfg }), {}),
+  },
   eslintConfigPrettier,
   {
-    ignores: ['dist/', 'node_modules/', '*.cjs'],
+    ignores: ['dist/', 'node_modules/', '*.cjs', '*.mjs', 'tsup.config.ts', 'eslint.config.js'],
   },
 ];
